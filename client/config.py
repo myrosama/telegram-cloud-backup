eabbd8d4d215e0244e13b02217123c298bfb5b69
myrosama <121111604+myrosama@users.noreply.github.com>
2025-06-14 18:40:58 +0500

    Update and rename config.json to config.py

---
+++ client/config.py
@@ -0,0 +1,13 @@
+
+# Get these from my.telegram.org
+API_ID = 27551777  # Replace with your API ID (it's a number)
+API_HASH = "e95d54fab963d5e374b9c5371a2a9b58" # Replace with your API Hash
+
+# The ID of the private Telegram channel where files will be uploaded.
+# It should be the numerical ID (e.g., -1001234567890)
+CHANNEL_ID = -1002446447298  # Replace with your Channel ID
+
+# Path to the file database used by the bot.
+# The client needs to write to this file after a successful upload.
+# The path is relative to the project root directory.
+DB_PATH = "file_db.json"
