ccdaa0811d96cfa409b9ad562539059307136d7e
myrosama <sadrikov49@gmail.com>
2025-06-15 16:06:26 +0500

    Final check

--- client/config.py
+++ client/config.py
@@ -5,11 +5,11 @@
 # 2. Click on "API development tools".
 # 3. Create a new app (you can call it anything).
 # 4. Copy the api_id and api_hash here.
-API_ID = 27523196  # Replace with your integer API ID
-API_HASH = "cbbcf2d8e08296c1e24bf3ab53b89787" # Replace with your string API Hash
+API_ID =   # Replace with your integer API ID
+API_HASH = "" # Replace with your string API Hash
 
 # --- REQUIRED: Channel ID ---
 # The ID of the private Telegram channel where files will be uploaded.
 # It should be the numerical ID (e.g., -1001234567890)
-CHANNEL_ID = -1002772637783 # Replace with your Channel ID
+CHANNEL_ID =  # Replace with your Channel ID
 
